fd52698aa9c2d53f38e9e4cf250042c45887db7b
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-25 18:30:12 -0400

    changed function and object structure

--- script.js
+++ script.js
@@ -1,78 +1,54 @@
-//holds the game board
-const gameBoard = (() => {
-    const drawBoard = () =>{
-        const pregrid = document.getElementById('grid')
-        const xButton = document.getElementById('x')
-        const oButton = document.getElementById('o')
-        xButton.remove()
-        oButton.remove()
-        pregrid.classList.remove('start-grid')
-        pregrid.classList.add('main-grid')
-        const grid = document.querySelector('.main-grid')
-        console.log(grid)
-        for(let i = 0; i<9; i++){
-            const cell = document.createElement('div')
-            cell.classList.add('cell')
-            grid.appendChild(cell)
-        } 
+(function(){
+    const Gameboard = {
+        squares: [[],[],[]],
+        drawBoard: function(){
+            const current = document.getElementById("grid")
+            current.classList.remove("start-grid")
+            current.classList.add("main-grid")
+            const grid = document.querySelector('.main-grid')
+            console.log(grid)
+            for(let i = 0; i<9; i++){
+                const cell = document.createElement('div')
+                cell.classList.add('cell')
+                grid.appendChild(cell)
+            }
+            gameLogic.playerMove()
+            console.log("yes")
+        }
     }
-    const updateBoard = (choice, symbol) => {
-        choice.target.innerHTML = symbol
+    
+    function player(symbol) {
+        return{
+            symbol
+        }
     }
-    return {drawBoard, updateBoard};
-  })();
 
-//holds the player
-const player = ((name, symbol) => {
-    const playerName = name
-    const playerSymbol = symbol
-    return {playerName, playerSymbol}
+    const gameLogic = {
+        chooseSymbol: function(){
+            const x = document.getElementById("x")
+            const o = document.getElementById("o")
+            o.addEventListener("click", e => {
+                const player1 = player(e.target.id)
+                x.remove()
+                o.remove()
+                Gameboard.drawBoard()
+            })
+            x.addEventListener("click", e => {
+                const player2 = player(e.target.id)
+                x.remove()
+                o.remove()
+                Gameboard.drawBoard()
+            })
+        },
+        playerMove: function(){
+            // add event listener to squares with 'cell' class.
+            const cells = document.getElementsByClassName('cell')
+            Array.from(cells).forEach(function(cell) {
+                cell.addEventListener('click', (e) => {
+                    Gameboard.addMove(e)
+                })
+              });
+        }
+    }
+    gameLogic.chooseSymbol()
 })()
-
-
-// controls the logic of the game
-const gameLogic = (() => {
-    const winner = ""
-    const board = gameBoard
-    const startGame = () => {
-        board.drawBoard();
-    }
-    const choosePlayer = () => {
-        const choice = document.querySelectorAll('.symbol-select');
-        choice.forEach((button) => {
-        button.addEventListener('click', (e) => {
-                const playerOne = player("Player One", e.target.id)
-                drawBoard()
-            });
-        });
-    }
-    const playTurn = () => {
-        document.addEventListener('click', (e) => {
-        console.log(e)
-        e.target.innerHTML = "X"
-        counter = 0
-        checkWin()
-        compTurn(counter)
-        })
-    }
-    const checkWin = () =>{
-        //check for any 3 aligned cells
-    }
-    return{startGame, winner, playTurn, choosePlayer, checkWin}
-})()
-
-
-/* const compTurn = (counter) => {
-    const grid = document.querySelectorAll('.cell')
-    const randomIndex = Math.floor(Math.random() * grid.length);
-    if(counter == 9){
-        gameOver()
-    }
-    else if(grid[randomIndex].innerHTML != ""){
-        counter++
-        compTurn(counter)
-    }
-    else{
-        grid[randomIndex].innerHTML = "O"
-    }
-} */
