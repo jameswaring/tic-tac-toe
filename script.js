8ab24d9b1dce21f093d2243a88d0b3c18b240b1f
James Waring <29707619+jameswaring@users.noreply.github.com>
2023-06-25 14:17:29 -0400

    functions moved around

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-const game = () => {
+//holds the game board
+const gameBoard = (() => {
     const drawBoard = () =>{
+        const pregrid = document.getElementById('grid')
+        const xButton = document.getElementById('x')
+        const oButton = document.getElementById('o')
+        xButton.remove()
+        oButton.remove()
+        pregrid.classList.remove('start-grid')
+        pregrid.classList.add('main-grid')
         const grid = document.querySelector('.main-grid')
         console.log(grid)
         for(let i = 0; i<9; i++){
             const cell = document.createElement('div')
-            cell.classList.add('.cell')
+            cell.classList.add('cell')
             grid.appendChild(cell)
         } 
+    }
+    const updateBoard = (choice, symbol) => {
+        choice.target.innerHTML = symbol
+    }
+    return {drawBoard, updateBoard};
+  })();
+
+//holds the player
+const player = ((name, symbol) => {
+    const playerName = name
+    const playerSymbol = symbol
+    return {playerName, playerSymbol}
+})()
+
+
+// controls the logic of the game
+const gameLogic = (() => {
+    const winner = ""
+    const board = gameBoard
+    const startGame = () => {
+        board.drawBoard();
+    }
+    const choosePlayer = () => {
+        const choice = document.querySelectorAll('.symbol-select');
+        choice.forEach((button) => {
+        button.addEventListener('click', (e) => {
+                const playerOne = player("Player One", e.target.id)
+                drawBoard()
+            });
+        });
+    }
+    const playTurn = () => {
+        document.addEventListener('click', (e) => {
+        console.log(e)
+        e.target.innerHTML = "X"
+        counter = 0
+        checkWin()
+        compTurn(counter)
+        })
     }
     const checkWin = () =>{
         //check for any 3 aligned cells
     }
-    return {drawBoard, checkWin};
-  };
-
-const newGame = game();
-newGame.drawBoard();
+    return{startGame, winner, playTurn, choosePlayer, checkWin}
+})()
 
 
-
-/* document.addEventListener('click', (e) => {
-    console.log(e)
-    e.target.innerHTML = "X"
-    counter = 0
-    //checkWin()
-    compTurn(counter)
-})
-
-
-const compTurn = (counter) => {
+/* const compTurn = (counter) => {
     const grid = document.querySelectorAll('.cell')
     const randomIndex = Math.floor(Math.random() * grid.length);
     if(counter == 9){
